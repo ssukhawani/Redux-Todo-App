491feb740d85b7fc2a885a9dff31c4fa899cf24d
ssukhawani <ssukhawani@gmail.com>
2021-03-11 21:38:14 +0530

    Added HashRouter insted BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducer/reducer";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Login from './components/Login/Login'
 import EditTodo from "./components/EditTodo/EditTodo";
 const { default: TodoApp } = require("./components/TodoApp/TodoApp");
@@ -22,13 +22,13 @@ function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <Switch>
             <Route exact path="/" component={Login} />
             <Route exact path="/TodoApp" component={TodoApp} />
             <Route path="/TodoApp/:id" component={EditTodo} />
           </Switch>
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </div>
   );
